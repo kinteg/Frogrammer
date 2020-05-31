466eda5827efea06331ee042458fcd91bda2a616
kinteg <zeroxchao@gmail.com>
2020-05-31 15:59:47 +0300

    Create Project

---
+++ src/main/java/com/kinteg/frogrammer/FrogrammerApplication.java
@@ -0,0 +1,13 @@
+package com.kinteg.frogrammer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogrammerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrogrammerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kinteg/frogrammer/FrogrammerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinteg.frogrammer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrogrammerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
